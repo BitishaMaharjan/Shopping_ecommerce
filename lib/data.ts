5335326b29aae8410e98574f6f76f2853ce650b6
bitisha <racerfire321@gmail.com>
2024-01-15 16:16:52 +0545

    -

---
+++ lib/data.ts
@@ -0,0 +1,80 @@
+export const data ={
+    products :[
+        {
+            name: "NIke",
+            slug: "Free shirt",      
+            image: "/images/shirt.jpg",
+            banner: "..",
+            price: 12,
+            brand: "Nike",
+            description: "Apopular shirt",
+            category: "shirt",
+            rating: 3.4,
+            numReviews: 4,
+            countInStock : 10,
+            
+        
+        },
+        {
+            name: "NIke",
+            slug: "Free shirt",      
+            image: "/images/shirt.jpg",
+            banner: "..",
+            price: 12,
+            brand: "Nike",
+            description: "Apopular shirt",
+            category: "shirt",
+            rating: 3.4,
+            numReviews: 4,
+            countInStock : 10,
+            
+        
+        },
+        {
+            name: "NIke",
+            slug: "Free shirt",      
+            image: "/images/shirt.jpg",
+            banner: "..",
+            price: 12,
+            brand: "Nike",
+            description: "Apopular shirt",
+            category: "shirt",
+            rating: 3.4,
+            numReviews: 4,
+            countInStock : 10,
+            
+        
+        },
+        {
+            name: "NIke",
+            slug: "Free shirt",      
+            image: "/images/shirt.jpg",
+            banner: "..",
+            price: 12,
+            brand: "Nike",
+            description: "Apopular shirt",
+            category: "shirt",
+            rating: 3.4,
+            numReviews: 4,
+            countInStock : 10,
+            
+        
+        },
+        {
+            name: "NIke",
+            slug: "Free shirt",      
+            image: "/images/shirt.jpg",
+            banner: "..",
+            price: 12,
+            brand: "Nike",
+            description: "Apopular shirt",
+            category: "shirt",
+            rating: 3.4,
+            numReviews: 4,
+            countInStock : 10,
+            
+        
+        }
+
+    ]
+}
